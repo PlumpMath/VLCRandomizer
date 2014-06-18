d0861e569d193215caf8415f205537e90a9145d4
Jonathan Poock <jonathanpoock@gmail.com>
2014-06-18 18:56:16 -0400

    content redesign, working towards initial test case

--- VLC.cs
+++ VLC.cs
@@ -8,9 +8,16 @@ namespace VLCRandomizer
 {
     class VLCRandomizer
     {
+        public VLCRandomizer(string vlcDirectory, string playlistName)
+        {
+            this.vlcDirectory = vlcDirectory;
+            this.playlistName = playlistName;
+        }
         public string vlcDirectory;
+        public string playlistName;
+        public int numItems;
         public List<ContentRoot> contents;
-        public List<ContentRoot> selectedContents
+        public List<ContentRoot> selectedRoot
         {
             get
             {
@@ -24,14 +31,19 @@ namespace VLCRandomizer
             get
             {
                 List<Content> output = new List<Content>();
-                foreach(ContentRoot c in this.selectedContents)
-                    output.AddRange(c.createContentsList());
+                foreach(ContentRoot c in this.selectedRoot)
+                    output.AddRange(c.selectedContentList());
 
                 return output;
             }
             set;
         }
-        public string playlistName;
+        public void AddRoot(string path)
+        {
+            ContentRoot newRoot = new ContentRoot(path);
+
+        }
+        
 
         public void generatePlaylist()
         {
@@ -51,29 +63,123 @@ namespace VLCRandomizer
         }
         public void Main()
         {
-            VLCRandomizer rando = new VLCRandomizer();
-            Random.
+            VLCRandomizer rando = new VLCRandomizer(vlcDirectory: "C:\\Program Files (x86)\\VideoLAN\\VLC", playlistName: "test.pls");
+            rando.AddRoot("Y:\\Television\\American Dad");
+            
         }
         
     }
     class ContentRoot
     {
         public string path;
-        public string rootName;
+        public string rootName
+        {
+            get
+            {
+                string[] delimiters = { "\\" };
+                string[] result = path.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                return result[result.Length - 1];
+            }
+            set;
+        }
         public bool isSelected;
         public List<Content> contents;
-        public List<Content> createContentsList()
+        public List<Content> selectedContentList()
         {
-            
-
             return contents.Where( s => (s as Content).isSelected == true).ToList() ;
         }
+        public List<Content> generateContentList()
+        {
+            string[] fileEntries = Directory.GetFiles(this.path);
+            foreach(string s in fileEntries)
+            {
+
+            }
+        }
+        public ContentRoot(string path)
+        {
+            this.path = path;
+            isSelected = false;
+        }
+        public bool isBottomLevel()
+        {
+            return false;
+        }
+    }
+    class ContentDivider : ContentRoot
+    {
+
     }
     class Content : ContentRoot
     {
-        public List<string> episodes;
+        public string name
+        {
+            get
+            {
+                string[] delimiters = { "\\" };
+                string[] result = path.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+                return result[result.Length - 1];
+            }
+            set;
+        }
+        public ContentRoot parent; 
+        public Content(string path, ContentRoot parent) : base(path)
+        {
+            this.parent = parent;
+        }
 
-    };
+
+    }
+    //struct AllDirectories
+    //{
+    //    public List<string> DirectoriesWithoutFiles { get; set; }
+    //    public List<string> DirectoriesWithFiles { get; set; }
+    //}
+
+    //static class FileSystemScanner
+    //{
+    //    public AllDirectories DivideDirectories(string startingPath)
+    //    {
+    //        var startingDir = new DirectoryInfo(startingPath);
+
+    //        // allContent IList<FileSystemInfo>
+    //        var allContent = GetAllFileSystemObjects(startingDir);
+    //        var allFiles = allContent.Where(f => !(f.Attributes & FileAttributes.Directory))
+    //                                 .Cast<FileInfo>();
+    //        var dirs = allContent.Where(f => (f.Attributes & FileAttributes.Directory))
+    //                             .Cast<DirectoryInfo>();
+    //        var allDirs = new SortedList<DirectoryInfo>(dirs, new FileSystemInfoComparer());
+
+    //        var res = new AllDirectories
+    //        {
+    //            DirectoriesWithFiles = new List<string>()
+    //        };
+    //        foreach (var file in allFiles)
+    //        {
+    //            var dirName = Path.GetDirectoryName(file.Name);
+    //            if (allDirs.Remove(dirName))
+    //            {
+    //                // Was removed, so first time this dir name seen.
+    //                res.DirectoriesWithFiles.Add(dirName);
+    //            }
+    //        }
+    //        // allDirs now just contains directories without files
+    //        res.DirectoriesWithoutFiles = new List<String>(addDirs.Select(d => d.Name));
+    //    }
+
+    //    class FileSystemInfoComparer : IComparer<FileSystemInfo>
+    //    {
+    //        public int Compare(FileSystemInfo l, FileSystemInfo r)
+    //        {
+    //            return String.Compare(l.Name, r.Name, StringComparison.OrdinalIgnoreCase);
+    //        }
+    //    }
+    //    IList<FileSystemInfo> GetAllFileSystemObjects(DirectoryInfo root)
+    //    {
+    //        return root.GetFileSystemInfos("*.*", SearchOptions.AllDirectories);
+    //    }
+    //}
+
     
     
 }
